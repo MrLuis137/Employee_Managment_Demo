93a93012f26354cf455b75b5cbbe393edab9f406
MrLuis137 <47502456+MrLuis137@users.noreply.github.com>
2022-05-27 16:26:19 -0500

    Base Proyect

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Enployee_Managment_Demo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
